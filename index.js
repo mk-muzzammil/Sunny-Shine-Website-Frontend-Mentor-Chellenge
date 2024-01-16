6d551e738efe9fcdd3873bc64e0482ac8a1eb784
Muhammad Muzzammil <muzzammil759@gmail.com>
2024-01-16 21:18:22 +0500

    First Commit Just oushing my code to github

---
+++ index.js
@@ -0,0 +1,14 @@
+const humbergerIcon=document.querySelector("#barIcon");
+
+
+
+humbergerIcon.addEventListener('click',()=>{
+
+
+
+    // write code for opening drower after humbergur icon is clicked 
+
+
+
+    console.log("Humburger clicked ");
+})
